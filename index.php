7882b5ff20ead11df06f55621769ca56ad8b1787
Xavierdl <Xav29dl@hotmail.com>
2017-08-29 14:31:31 -0400

    Revert "Initial commit"
    This reverts commit b1ca2ccf5a8599c6fa596cbdcbed0b9dce239c27.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<title>Document</title>
 </head>
 <body>
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi tempore qui corrupti necessitatibus recusandae dolores unde numquam veniam sunt rem, illum nam, iste possimus assumenda itaque natus eos quidem voluptatibus.
-	<div class="conte"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, cumque, facere! Veritatis magnam cum dignissimos iure animi laudantium, vero labore harum incidunt quisquam voluptas, odit reprehenderit. Praesentium consectetur vero sint.</p></div>
+	<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi tempore qui corrupti necessitatibus recusandae dolores unde numquam veniam sunt rem, illum nam, iste possimus assumenda itaque natus eos quidem voluptatibus.
+	</div>
 </body>
 </html>
